23fc64f002fdce2f7882cc44eacac6dcf71a22e9
Ishan Rohatgi <ishan.77775@gmail.com>
2024-04-25 20:31:09 +0530

    increased digit size

--- script.js
+++ script.js
@@ -18,32 +18,32 @@ function setTimePosition(){
     let secTens = document.getElementsByClassName('sec-tens')[0];
     let secOnes = document.getElementsByClassName('sec-ones')[0];
     if(hour<=9){
-        hourOnes.style.top = `-${hour * 39}px`;
+        hourOnes.style.top = `-${hour * 49}px`;
         hourTens.style.top = `0px`;
     }else{
         let hourValue = hour.toString().split('');
         let [hourTensDigit, hourOnesDigit] =  hourValue;
-        hourTens.style.top = `-${hourTensDigit*39}px`;
-        hourOnes.style.top = `-${hourOnesDigit*39}px`;
+        hourTens.style.top = `-${hourTensDigit*49}px`;
+        hourOnes.style.top = `-${hourOnesDigit*49}px`;
     }
     if(min<=9){
-        minOnes.style.top = `-${min * 39}px`;
+        minOnes.style.top = `-${min * 49}px`;
         minTens.style.top = `0px`;
     }else{
         let minValue = min.toString().split('');
         let [minTensDigit, minOnesDigit] =  minValue;
-        minTens.style.top = `-${minTensDigit*39}px`;
-        minOnes.style.top = `-${minOnesDigit*39}px`;
+        minTens.style.top = `-${minTensDigit*49}px`;
+        minOnes.style.top = `-${minOnesDigit*49}px`;
     }
 
     if(sec<=9){
-        secOnes.style.top = `-${sec * 39}px`;
+        secOnes.style.top = `-${sec * 49}px`;
         secTens.style.top = `0px`;
     }else{
         let secValue = sec.toString().split('');
         let [secTensDigit, secOnesDigit] =  secValue;
-        secTens.style.top = `-${secTensDigit*39}px`;
-        secOnes.style.top = `-${secOnesDigit*39}px`;
+        secTens.style.top = `-${secTensDigit*49}px`;
+        secOnes.style.top = `-${secOnesDigit*49}px`;
     }
          
 
